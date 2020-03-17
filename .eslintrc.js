e94bba06f3b6ef624c9a3dad4e6b1e83a4ca7fc2
Albert Luta <luta.albert100@gmail.com>
2020-03-17 22:13:39 +0200

    Updated the formating options and formatted all files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,14 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
+	root: true,
+	env: { node: true },
+	extends: ["plugin:vue/essential", "@vue/airbnb"],
+	parserOptions: { parser: "babel-eslint" },
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+		indent: ["error", "tab"],
+		quotes: ["error", "double"],
+		"comma-dangle": ["error", "never"],
+		"no-tabs": ["error", { allowIndentationTabs: true }]
+	}
 };
