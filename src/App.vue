65feacbc7dca19653a6df4e20627aa48d93003a9
Albert Luta <luta.albert100@gmail.com>
2020-03-18 23:18:46 +0200

    Added the global style

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,20 @@
 	<router-view></router-view>
 </template>
 
-<script>
-export default {};
-</script>
+<style>
+*,
+*::before,
+*::after {
+	box-sizing: border-box;
+}
+:root {
+	--clr-blue: #4a90e2;
+	--clr-red: #ff3b30;
+	--clr-gold: #e8b100;
 
-<style></style>
+	font-size: 62.5%;
+}
+body {
+	font: 1.6rem/1.5 sans-serif;
+}
+</style>
