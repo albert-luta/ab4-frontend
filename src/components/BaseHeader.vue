3a163174f16e893a9f5235697fa0f3367c551c3e
Albert Luta <luta.albert100@gmail.com>
2020-03-22 20:26:33 +0200

    Created BaseHeader component

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,40 @@
+<template>
+	<header>
+		<div class="container base-header__content-wrapper">
+			<section class="base-header__logo">
+				<h1>Kite</h1>
+			</section>
+			<section class="base-header__content">
+				<slot />
+			</section>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name: 'BaseHeader'
+};
+</script>
+
+<style scoped>
+.base-header__content-wrapper {
+	display: flex;
+	align-items: center;
+
+	padding: 1.2rem 0;
+}
+
+.base-header__logo {
+	padding-right: 2rem;
+}
+
+.base-header__logo h1 {
+	font-size: 2.8rem;
+	margin: 0;
+}
+
+.base-header__content {
+	flex: 1;
+}
+</style>
