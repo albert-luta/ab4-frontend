f6e883b0caaf87837de16d0595e4aa926726ef27
Albert Luta <luta.albert100@gmail.com>
2020-03-23 03:17:11 +0200

    Created and implemented DashboardHeader

---
+++ src/components/DashboardHeader.vue
@@ -0,0 +1,34 @@
+<template>
+	<BaseHeader>
+		<div class="dashboard-header">
+			<BaseButton title="ADD SPOT" class="dashboard-header__add-spot" />
+			<TheAccountMenu class="dashboard-header__account" />
+		</div>
+	</BaseHeader>
+</template>
+
+<script>
+import TheAccountMenu from './TheAccountMenu.vue';
+
+export default {
+	name: 'DashboardHeader',
+	components: {
+		TheAccountMenu
+	}
+};
+</script>
+
+<style scoped>
+.dashboard-header {
+	display: flex;
+	justify-content: flex-end;
+}
+
+.dashboard-header__add-spot {
+	font-size: 1.4rem;
+}
+
+.dashboard-header__account {
+	margin-left: 5rem;
+}
+</style>
